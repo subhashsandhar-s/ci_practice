7a4c65d5ff370bf4e7a561f7419236316841515d
subhashsandhars <subhashsandhars@gmail.com>
2025-02-06 23:55:25 +0530

    Generating error to check for pr merge

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	actual := Add(2, 3)
-	expected := 10
+	expected := 11
 	if actual != expected {
 		t.Errorf("Expected %d, got %d", expected, actual)
 	}
